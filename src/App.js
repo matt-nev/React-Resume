955249e95464349844ef434ab2519698443c7c72
Matt Nev <mattnev@microsoft.com>
2021-11-20 13:48:56 -0600

    recreate project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.module.css';
+import Resume from './Resume/Resume.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Resume />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Resume/Resume.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import styles from './Resume.module.css';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <img src="/Resume.jpg" alt="Resume" />
+      </div>
+    );
+  }
+}

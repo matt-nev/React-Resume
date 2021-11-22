3839b23fdb3eb7241d87b5365b049d8a7c6fee18
Matt Nev <mattnev@microsoft.com>
2021-11-22 11:56:51 -0600

    add hover effect

--- src/Resume/Resume.js
+++ src/Resume/Resume.js
@@ -1,12 +1,15 @@
 import React from 'react';
-// import styles from './Resume.module.css';
+import styles from './Resume.module.css';
 
 export default class App extends React.Component {
   render() {
     return (
+    <div class={styles.container}>
       <div>
-        <img src="/Resume.jpg" alt="Resume" />
+        <img src="/Resume.jpg" alt="Resume" class={styles.image} />
+        <div class={styles.overlay}> Hire me pls </div>
       </div>
+    </div>
     );
   }
 }
